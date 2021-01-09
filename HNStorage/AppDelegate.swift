3fc7131b6a9c09908c6ab0d3cf14159631ff8051
Hady <hady.nourallah@gmail.com>
2021-01-09 14:10:26 +0100

    remove bad code

--- HNStorage/AppDelegate.swift
+++ HNStorage/AppDelegate.swift
@@ -10,20 +10,10 @@ import HNStorage
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    struct Str: Storable, Codable {
-        var primaryKey: String {
-            name
-        }
-        
-        let name: String
-    }
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let s = HNStorage<HNUserDefaultsStorage>.init()
-        
-//        try? HNStorage<HNUserDefaultsStorage>().insert(Str(name: "domethig"))
+
         return true
     }
     
